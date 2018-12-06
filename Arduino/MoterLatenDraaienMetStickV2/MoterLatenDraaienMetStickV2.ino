f0fca9c162c33446c0e2472fe39efb8bf6fbbb45
Gürkan <gurkan_@live.nl>
2018-12-06 12:46:42 +0100

    Servo
    De Servo wordt aangesloten op het output van de Digital pin 9.

--- Arduino/MoterLatenDraaienMetStickV2/MoterLatenDraaienMetStickV2.ino
+++ Arduino/MoterLatenDraaienMetStickV2/MoterLatenDraaienMetStickV2.ino
@@ -3,3 +3,8 @@
 Servo myservo;  // creates servo object to control a servo
 
 int val;    // variable to read the value from the analog pin
+
+void setup()
+{
+  myservo.attach(9);  // ensures output to servo on pin 9
+}
