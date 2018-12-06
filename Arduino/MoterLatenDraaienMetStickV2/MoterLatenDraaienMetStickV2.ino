1ad8727ab9d5faafc41a47595537794b93e3c8b4
Gürkan <gurkan_@live.nl>
2018-12-06 12:44:42 +0100

    Begin aan bestand
    Eerst een access naar de Arduino Servo Bibliotheek, vervolgens maak ik een servo object aan die de controle van een servo(arduino) aangaat.

---
+++ Arduino/MoterLatenDraaienMetStickV2/MoterLatenDraaienMetStickV2.ino
@@ -0,0 +1,5 @@
+#include <Servo.h> //accesses the Arduino Servo Library
+
+Servo myservo;  // creates servo object to control a servo
+
+int val;    // variable to read the value from the analog pin
