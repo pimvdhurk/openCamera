ee01d740a4feda75d690c7adeef6a63b203ebe8d
Gürkan <gurkan_@live.nl>
2018-12-06 12:17:41 +0100

    Setup en loop
    Begin aan de setup (draait 1x). En de loop, hier komt de code te staan wat hij moet uitvoeren en steeds moet herhalen.

--- Arduino/MoterLatenDraaienMetStick/MoterLatenDraaienMetStick.ino
+++ Arduino/MoterLatenDraaienMetStick/MoterLatenDraaienMetStick.ino
@@ -8,3 +8,16 @@ int angleMax = 120;
 int sensePin = 0;
 
 bool isUp = true;
+
+void setup() {
+  // put your setup code here, to run once:
+  servo.attach(8);
+//  servo.write(angleMax);
+  Serial.begin(9600);
+}
+
+void loop() {
+  double newAnalog = analogRead(sensePin);
+  Serial.println(newAnalog);
+  newAnalog = newAnalog / 1023;
+  newAnalog = newAnalog * 100;
