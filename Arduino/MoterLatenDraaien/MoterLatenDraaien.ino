7ae44d7a89dc475027c3684362f069a9b53d3b70
Gürkan <gurkan_@live.nl>
2018-12-06 11:36:49 +0100

    0 -> 180 / 180 -> 0
    Code toegevoegd waarbij het motertje van 0 graden naar 180 graden gaat en terug van 180 graden naar 0 graden.

--- Arduino/MoterLatenDraaien/MoterLatenDraaien.ino
+++ Arduino/MoterLatenDraaien/MoterLatenDraaien.ino
@@ -8,3 +8,19 @@ void setup() {
   servo.attach(8);
   servo.write(angle);
 }
+
+void loop() {
+  //scan van 0 tot 180 graden
+    for(angle = 10; angle < 180; angle++)
+    {
+      servo.write(angle);
+      delay(15);
+    }
+
+    //nu scan terug van 180 naar 0 graden
+    for(angle = 180; angle > 10; angle--)
+    {
+      servo.write(angle);
+      delay(15);  
+    }
+}
