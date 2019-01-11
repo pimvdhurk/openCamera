73535dc83ee57b66a577ad54033c5c8a1a7c8fe9
Unknown <p.vandenhurk@student.fontys.nl>
2019-01-11 10:04:15 +0100

    info toegevoegd

--- beeld/opacity.js
+++ beeld/opacity.js
@@ -1,5 +1,6 @@
 var doorzichtigheid = 3;
 
+kleurAanpassen();
 
 function donkerder() {
 
@@ -35,13 +36,15 @@ function kleurAanpassen() {
 
 if (doorzichtigheid == 3) {
 	document.getElementById('blackCover').style.backgroundColor = 'rgba(0, 0, 0, 1)';
+	document.getElementById('infoText').textContent = 'Probeer de spiegels goed te zetten. Hierdoor zal er licht vanuit de lens naar de zoeker worden geleid.';
 }
 else if (doorzichtigheid == 2) {
 	document.getElementById('blackCover').style.backgroundColor = 'rgba(0, 0, 0, 0.5)';
-	console.log('check');
+	document.getElementById('infoText').textContent = 'De eerste spiegel staat goed! Er komt al meer licht door de zoeker.';
 }
 else {
 	document.getElementById('blackCover').style.backgroundColor = 'rgba(0, 0, 0, 0)';
+	document.getElementById('infoText').textContent = 'Allebei de spiegels staan goed. Je ziet nu precies wat de camera fotografeert.';
 }
 
 }
